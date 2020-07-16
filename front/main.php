c94753460058e80cacc55373de33d539e90b0a4c
Loki <summer10920@gmail.com>
2020-07-16 14:17:58 +0800

    建置後台院線片管理版面

--- front/main.php
+++ front/main.php
@@ -128,9 +128,9 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
             $(dom).animate({width:0,height:0,left:100,top:130},function(){
                 $(next).css({width:0,height:0,left:100,top:130})
                 $(next).show();
-                $(next).animate({width:200,height:200,left:0,top:0})
+                $(next).animate({width:200,height:260,left:0,top:0})
                 $(dom).hide()
-                $(dom).css({width:200,height:200,left:0,top:0})
+                $(dom).css({width:200,height:260,left:0,top:0})
             })
 
         }
