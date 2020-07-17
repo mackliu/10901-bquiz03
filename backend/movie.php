9d4b8b5530d685359b3b6e910f709e2b190d08a9
Loki <summer10920@gmail.com>
2020-07-17 08:53:41 +0800

    完成後台院線片管理的排序功能及刪除電影功能

--- backend/movie.php
+++ backend/movie.php
@@ -36,6 +36,9 @@
 $db=new DB('movie');
 $rows=$db->all([]," order by rank");
 foreach($rows as $k => $row){
+        //找出排序後的上一筆和下一筆的資料id
+        $prev=($k!=0)?$rows[$k-1]['id']:$row['id'];
+        $next=($k!=(count($rows)-1))?$rows[$k+1]['id']:$row['id'];
 ?>
 <div class="movie-item">
     <div>
@@ -46,7 +49,13 @@ foreach($rows as $k => $row){
             <span>片長:<?=$row['length'];?></span>
             <span>上映時間:<?=$row['ondate'];?></span>
         </div>
-        <div>功能按鈕</div>
+        <div>
+            <button onclick="sh('movie',<?=$row['id'];?>)">顯示</button>
+            <button class="shift" data-rank="<?=$row['id']."-".$prev;?>">往上</button>
+            <button class="shift" data-rank="<?=$row['id']."-".$next;?>">往下</button>
+            <button onclick="edit('movie',<?=$row['id'];?>)">編輯電影</button>
+            <button onclick="del('movie',<?=$row['id'];?>)">刪除電影</button>
+        </div>
         <div>劇情簡介:<?=$row['intro'];?></div>
     </div>
 </div>
@@ -56,3 +65,30 @@ foreach($rows as $k => $row){
 
 
 </div>
+
+<script>
+
+function del(movie,id){
+    $.post("api/del.php",{"table":movie,"id":id},function(){
+        location.reload();
+    })
+}
+
+
+//使用jquery來對button的點擊做處理
+$(".shift").on("click",function(){
+
+    //取得data屬性的值，並拆成一個id陣列
+    let id=$(this).data("rank").split("-");
+
+    //將id陣列連同要修改的資料表名稱一起用ajax的方式一直傳到後台
+    $.post("api/rank.php",{id,"table":"movie"},function(){
+
+        //後台api處理完畢後重新載入一次頁面
+        location.reload();
+    })
+})
+
+
+
+</script>
